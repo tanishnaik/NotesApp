da31cebc541b05cca8ba545f709cca5e7c857c7f
Tanish <tanishop13062005@gmail.com>
2025-02-27 19:08:34 +0530

    inital commit

---
+++ notesApp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <h1 class="text-cyan-400 ">
+          <h2 class='text-white'>Tanish</h2>
+          <p class='text-red-200'>Vaishnav</p>
+    Hello world!
+  </h1>
+
+    </>
+  )
+}
+
+export default App
